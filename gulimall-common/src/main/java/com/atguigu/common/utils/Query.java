b85da1d2e1f7db3925b3b0bd78263f0d4d70c8c7
uname <2575939509@qq.com>
2025-03-13 22:31:28 +0800

    第二天

--- gulimall-common/src/main/java/com/atguigu/common/utils/Query.java
+++ gulimall-common/src/main/java/com/atguigu/common/utils/Query.java
@@ -11,7 +11,7 @@ package com.atguigu.common.utils;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import io.renren.common.xss.SQLFilter;
+import com.atguigu.common.xss.SQLFilter;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.Map;
